d8170d9a3bf66e6eecb9c3ec21d9f58d1f7980c3
funnyappnoreply-dotcom <funnyapp.noreply@gmail.com>
2026-04-05 00:03:27 -0300

    Update posts.js

--- backend/src/routes/posts.js
+++ backend/src/routes/posts.js
@@ -12,11 +12,10 @@ router.get('/trending', trending)
 router.get('/seguindo', autenticar, feedSeguindo)
 router.get('/usuario/:username', postsPorUsuario)
 router.get('/:id', autenticarOpcional, buscarPorId)
-router.post('/', autenticar, uploadMidia.single('midia'), criar) // aceita imagem ou vídeo
+router.post('/', autenticar, uploadMidia.any(), criar)
 router.delete('/:id', autenticar, deletar)
 router.post('/:id/curtir', autenticar, curtir)
 router.post('/:id/comentarios', autenticar, comentar)
 router.delete('/:id/comentarios/:cid', autenticar, deletarComentario)
 router.post('/:id/denunciar', autenticar, denunciar)
-
 module.exports = router
